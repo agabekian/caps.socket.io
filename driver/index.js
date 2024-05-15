dedd97272a1c4228cf1070b275d3e68a72914753
Armen Agabekian <2008nv@gmail.com>
2024-05-14 17:44:06 -0700

    fix-vendor-thanks-id

--- driver/index.js
+++ driver/index.js
@@ -1,9 +1,6 @@
-
 const io = require('socket.io-client');
-// let serverURL = "https://872d-50-47-110-82.ngrok-free.app";
-let serverURL = "http://localhost:3000";
+let serverURL = process.env.SERVER_URL||"http://localhost:3000";
 const socket = io.connect(serverURL);
-
 
 socket.on('pickup', payload => {
     console.log(`pickup ${payload.orderId}`)
